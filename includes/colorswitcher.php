1eebec0779d9e3535d95715f71348d6555258396
Barani Kumar <125998398+B-A-R-A-N-I@users.noreply.github.com>
2024-11-08 21:47:40 +0530

    Add files via upload

---
+++ includes/colorswitcher.php
@@ -0,0 +1,17 @@
+<div class="switcher-wrapper">	
+    <div class="demo_changer">
+        <div class="demo-icon customBgColor"><i class="fa fa-cog fa-spin fa-2x"></i></div>
+        <div class="form_holder">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="predefined_styles">
+                        <div class="skin-theme-switcher">
+                            <h4>Color</h4>
+                            <a href="#" data-switchcolor="blue" class="styleswitch" style="background-color:#228dcb;"> </a>                       
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,54 @@
+
+<header>
+  <div class="default-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-md-2">
+          <div class="logo"> <a href="index.php"><img src="assets/images/redcar.png" alt="image"/>CAR RENTS</a> </div>
+        </div>
+        <div class="col-sm-9 col-md-10">
+          <div class="header_info">
+         <?php
+         $sql = "SELECT EmailId,ContactNo from tblcontactusinfo";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+foreach ($results as $result) {
+$email=$result->EmailId;
+$contactno=$result->ContactNo;
+}
+?>           
+   <?php   if(strlen($_SESSION['login'])==0)
+	{	
+?>
+ <div class="login_btn"> <a href="#loginform" class="btn btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a> </div>
+<?php }
+else{ 
+
+echo "Welcome To Car rental portal";
+ } ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Navigation -->
+  <nav id="navigation_bar" class="navbar navbar-default">
+    <div class="container">
+      
+      <div class="collapse navbar-collapse" id="navigation">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">Home</a></li>	 
+          <li><a href="page.php?type=aboutus">About Us</a></li>
+          <li><a href="car-listing.php">Car Listing</a>
+          <li><a href="page.php?type=faqs">FAQs</a></li>
+          <li><a href="contact-us.php">Contact Us</a></li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Navigation end --> 
+  
+</header>
